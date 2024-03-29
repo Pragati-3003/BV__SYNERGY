b5ee2378a417c99add60b06280d6fc50092a073b
Shubha274 <pandeyshubha27@gmail.com>
2024-03-29 08:07:49 +0530

    event updated

--- client/src/components/events/Events.jsx
+++ client/src/components/events/Events.jsx
@@ -1,49 +1,103 @@
-import React from "react";
-import TextField from "@mui/material/TextField";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
-import Badge from "@mui/material/Badge";
-import { PickersDay } from "@mui/x-date-pickers/PickersDay";
-import CheckIcon from "@mui/icons-material/Check";
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 import "./events.css";
-const Events = () => {
-  const [value, setValue] = useState(new Date());
-  const [highlightedDays, setHighlightedDays] = useState([1, 2, 13]);
-  return (
-    <div className="event">
-      <h1 className="header-text">Event Calendar </h1>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <StaticDatePicker
-          // mask='____/__/__'
-          variant="static"
-          orientation="portrait"
-          value={value}
-          //disableFuture
-          onChange={(newValue) => setValue(newValue)}
-          renderInput={(params) => {
-            <TextField {...params} />;
-          }}
-          renderDay={(day, _value, DayComponentProps) => {
-            const isSelected =
-              !DayComponentProps.outsideCurrentMonth &&
-              highlightedDays.indexOf(day.getDate()) >= 0;
+const locales = {
+  "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 
-            return (
-              <Badge
-                key={day.toString()}
-                overlap="circular"
-                badgeContent={
-                  isSelected ? <CheckIcon color="red" /> : undefined
-                }
-              >
-                <PickersDay {...DayComponentProps} />
-              </Badge>
-            );
-          }}
+const events = [
+  {
+    title: "Mayukh",
+    allDay: true,
+    start: new Date(2024, 3, 21),
+    end: new Date(2024, 3, 23),
+  },
+  {
+    title: "WeHack",
+    start: new Date(2024, 3, 2),
+    end: new Date(2024, 2, 3),
+  },
+  {
+    title: "Periodicals",
+    start: new Date(2024, 3, 15),
+    end: new Date(2024, 3, 18),
+  },
+];
+const Events = () => {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [allEvents, setAllEvents] = useState(events);
+
+  function handleAddEvent() {
+    for (let i = 0; i < allEvents.length; i++) {
+      const d1 = new Date(allEvents[i].start);
+      const d2 = new Date(newEvent.start);
+      const d3 = new Date(allEvents[i].end);
+      const d4 = new Date(newEvent.end);
+      /*
+      console.log(d1 <= d2);
+      console.log(d2 <= d3);
+      console.log(d1 <= d4);
+      console.log(d4 <= d3);
+        */
+
+      if ((d1 <= d2 && d2 <= d3) || (d1 <= d4 && d4 <= d3)) {
+        alert("CLASH");
+        break;
+      }
+    }
+
+    setAllEvents([...allEvents, newEvent]);
+  }
+
+  return (
+    <div className="events">
+      <h1>Calendar</h1>
+      <h2>Add New Event</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="Add Title"
+          style={{ width: "20%", marginRight: "10px" }}
+          value={newEvent.title}
+          onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
         />
-      </LocalizationProvider>
+        <DatePicker
+          placeholderText="Start Date"
+          style={{ marginRight: "10px" }}
+          selected={newEvent.start}
+          onChange={(start) => setNewEvent({ ...newEvent, start })}
+        />
+        <DatePicker
+          placeholderText="End Date"
+          selected={newEvent.end}
+          onChange={(end) => setNewEvent({ ...newEvent, end })}
+        />
+        <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+          Add Event
+        </button>
+      </div>
+      <Calendar
+        localizer={localizer}
+        events={allEvents}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500, margin: "50px" }}
+      />
     </div>
   );
 };
