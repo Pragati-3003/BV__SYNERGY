dae7b2c3e914f59a3c92235a700b4c9f581bd811
[200~mohiniruhela~ <ruhelamohini@gmail.com>
2024-03-29 15:35:09 +0530

    department page added

--- client/src/components/departments/Departments.jsx
+++ client/src/components/departments/Departments.jsx
@@ -1,8 +1,337 @@
 import React from 'react'
 import './departments.css'
+import Topbar from "../../components/topbar/Topbar";
 const Departments = () => {
   return (
-    <div>Departments</div>
+    <> <Topbar />
+
+<section className="main-section">
+       <div className="main-container">
+      <div className="heading">
+        <div className="heading-in">Departments Information</div></div>
+
+        <div className="dept-main-card">
+        <div className="department-cards">
+      <div class="card__container">
+            
+            
+            <article class="card__article">
+               <img src="assets/dept_img/automation.jpeg" alt="image" class="card__img"></img>
+               <div class="card__data">
+                  <span class="card__description">Electrical and Electronics,Electronics and Instrumentation</span>
+                  <h2 class="card__title">School of Automation </h2>
+                  <a href="#" class="card__button">The School of Automation at Banasthali is quintessence of the Universitys relentless pursuit of excellence, where the students are exposed to real-world situations.</a>
+               </div>
+               <div className="cd-hd">
+               <h2 class="card__title-1">School of Automation </h2>
+               </div>
+               
+               </article>
+
+
+
+               <article class="card__article">
+               <img src="assets/dept_img/aapaji.jpeg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">Center For Mathematical Science</span>
+                  <h2 class="card__title"> Aapaji Sansthan</h2>
+                  <a href="#" class="card__button">Computer Science is the core of Faculty of Mathematics and Computing. The department operates from the building "ICICI Centre for Advanced Studies in Computer Science". This building has been constructed from the generous financial assistance from ICICI.</a>
+               </div>
+               
+
+               <div className="cd-hd">
+               <h2 class="card__title-1"> Aapaji Sansthan</h2>
+               </div>
+               </article>
+
+
+               
+
+               
+               <article class="card__article">
+               <img src="assets/dept_img/vidhi.jpeg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">B.A.LL.B., LL.B. (BBA) that is B.B.A.LL.B. and LL.B. (B.Com.)</span>
+                  <h2 class="card__title">Vidhi Mandir (DEPARTMENT OF LEGAL STUDIES) </h2>
+                  <a href="#" class="card__button">The Jamnalal Bajaj Foundation very kindly consented to give a onetime philanthropic grant to partially meet the expenses of the Law School Building, named as Jamnalal Bajaj School of Legal Studies.</a>
+               </div>
+              
+
+               <div className="cd-hd">
+               <h2 class="card__title-1"> Vidhi Mandir</h2>
+               </div> 
+
+
+               </article>
+
+
+               <article class="card__article">
+               <img src="assets/dept_img/pharmacy.jpeg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">Ph.D. (Pharmacy) of Pharmacy (M.Pharm. - Pharmaceutical Chemistry),Master of Pharmacy (M.Pharm. - Pharmaceutics),Master of Pharmacy (M.Pharm. - Pharmacology) ,Bachelor of Pharmacy (B.Pharm.)</span>
+                  <h2 class="card__title">Department of Pharmacy </h2>
+                  <a href="#" class="card__button">Department has undying passion for research and most of the faculty members are engaged in research activities which includes synthesis, identification and optimization of lead compounds, development of biological assay methods and formulation development of drugs for better pharmacokinetics and dynamics.</a>
+               </div>
+              
+
+               <div className="cd-hd">
+               <h2 class="card__title-1">Department of Pharmacy </h2>
+               </div> 
+               </article>
+
+               <article class="card__article">
+               <img src="assets/dept_img/architecture.jpg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">B. Arch (Bachelor of Architecture)</span>
+                  <h2 class="card__title">School of Architecture & Planning </h2>
+                  <a href="#" class="card__button">The School of Architecture& Planning is a new venture at Banasthali Vidyapith offering a full-time 5-year degree program, approved by COUNCIL OF ARCHITECTURE, New Delhi. </a>
+               </div>
+             
+               <div className="cd-hd">
+               <h2 class="card__title-1">School of Architecture  </h2>
+
+               </div> 
+               </article>
+
+
+               <article class="card__article">
+               <img src="assets/dept_img/automation.jpeg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">Electrical and Electronics,Electronics and Instrumentation</span>
+                  <h2 class="card__title">School of Automation </h2>
+                  <a href="#" class="card__button">The School of Automation at Banasthali is quintessence of the Universitys relentless pursuit of excellence, where the students are exposed to real-world situations.</a>
+               </div>
+               
+
+               
+               <div className="cd-hd">
+               <h2 class="card__title-1">School of Automation </h2>
+
+               </div> 
+               </article>
+
+
+
+               <article class="card__article">
+               <img src="assets/dept_img/automation.jpeg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">Electrical and Electronics,Electronics and Instrumentation</span>
+                  <h2 class="card__title">School of Automation </h2>
+                  <a href="#" class="card__button">The School of Automation at Banasthali is quintessence of the Universitys relentless pursuit of excellence, where the students are exposed to real-world situations.</a>
+               </div>
+               
+               <div className="cd-hd">
+               <h2 class="card__title-1">School of Automation </h2>
+
+               </div> 
+               </article>
+
+
+
+               <article class="card__article">
+               <img src="assets/dept_img/aapaji.jpeg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">Center For Mathematical Science</span>
+                  <h2 class="card__title"> Aapaji Sansthan</h2>
+                  <a href="#" class="card__button">Computer Science is the core of Faculty of Mathematics and Computing. The department operates from the building "ICICI Centre for Advanced Studies in Computer Science". This building has been constructed from the generous financial assistance from ICICI.</a>
+               </div>
+              
+
+               <div className="cd-hd">
+               <h2 class="card__title-1"> Aapaji Sansthan</h2>
+
+               </div> 
+
+               </article>
+
+
+               <article class="card__article">
+               <img src="assets/dept_img/automation.jpeg" alt="image" class="card__img"></img>
+               <div class="card__data">
+                  <span class="card__description">Electrical and Electronics,Electronics and Instrumentation</span>
+                  <h2 class="card__title">School of Automation </h2>
+                  <a href="#" class="card__button">The School of Automation at Banasthali is quintessence of the Universitys relentless pursuit of excellence, where the students are exposed to real-world situations.</a>
+               </div>
+               <div className="cd-hd">
+               <h2 class="card__title-1">School of Automation </h2>
+               </div>
+               
+               </article>
+
+
+
+               <article class="card__article">
+               <img src="assets/dept_img/aapaji.jpeg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">Center For Mathematical Science</span>
+                  <h2 class="card__title"> Aapaji Sansthan</h2>
+                  <a href="#" class="card__button">Computer Science is the core of Faculty of Mathematics and Computing. The department operates from the building "ICICI Centre for Advanced Studies in Computer Science". This building has been constructed from the generous financial assistance from ICICI.</a>
+               </div>
+               
+
+               <div className="cd-hd">
+               <h2 class="card__title-1"> Aapaji Sansthan</h2>
+               </div>
+               </article>
+
+
+           
+
+               
+               <article class="card__article">
+               <img src="assets/dept_img/vidhi.jpeg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">B.A.LL.B., LL.B. (BBA) that is B.B.A.LL.B. and LL.B. (B.Com.)</span>
+                  <h2 class="card__title">Vidhi Mandir (DEPARTMENT OF LEGAL STUDIES) </h2>
+                  <a href="#" class="card__button">The Jamnalal Bajaj Foundation very kindly consented to give a onetime philanthropic grant to partially meet the expenses of the Law School Building, named as Jamnalal Bajaj School of Legal Studies.</a>
+               </div>
+              
+
+               <div className="cd-hd">
+               <h2 class="card__title-1"> Vidhi Mandir</h2>
+               </div> 
+
+
+               </article>
+
+
+               <article class="card__article">
+               <img src="assets/dept_img/pharmacy.jpeg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">Ph.D. (Pharmacy) of Pharmacy (M.Pharm. - Pharmaceutical Chemistry),Master of Pharmacy (M.Pharm. - Pharmaceutics),Master of Pharmacy (M.Pharm. - Pharmacology) ,Bachelor of Pharmacy (B.Pharm.)</span>
+                  <h2 class="card__title">Department of Pharmacy </h2>
+                  <a href="#" class="card__button">Department has undying passion for research and most of the faculty members are engaged in research activities which includes synthesis, identification and optimization of lead compounds, development of biological assay methods and formulation development of drugs for better pharmacokinetics and dynamics.</a>
+               </div>
+              
+
+               <div className="cd-hd">
+               <h2 class="card__title-1">Department of Pharmacy </h2>
+               </div> 
+               </article>
+
+               <article class="card__article">
+               <img src="assets/dept_img/architecture.jpg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">B. Arch (Bachelor of Architecture)</span>
+                  <h2 class="card__title">School of Architecture & Planning </h2>
+                  <a href="#" class="card__button">The School of Architecture& Planning is a new venture at Banasthali Vidyapith offering a full-time 5-year degree program, approved by COUNCIL OF ARCHITECTURE, New Delhi. </a>
+               </div>
+             
+               <div className="cd-hd">
+               <h2 class="card__title-1">School of Architecture  </h2>
+
+               </div> 
+               </article>
+
+
+               <article class="card__article">
+               <img src="assets/dept_img/automation.jpeg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">Electrical and Electronics,Electronics and Instrumentation</span>
+                  <h2 class="card__title">School of Automation </h2>
+                  <a href="#" class="card__button">The School of Automation at Banasthali is quintessence of the Universitys relentless pursuit of excellence, where the students are exposed to real-world situations.</a>
+               </div>
+               
+
+               
+               <div className="cd-hd">
+               <h2 class="card__title-1">School of Automation </h2>
+
+               </div> 
+               </article>
+
+
+
+               <article class="card__article">
+               <img src="assets/dept_img/automation.jpeg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">Electrical and Electronics,Electronics and Instrumentation</span>
+                  <h2 class="card__title">School of Automation </h2>
+                  <a href="#" class="card__button">The School of Automation at Banasthali is quintessence of the Universitys relentless pursuit of excellence, where the students are exposed to real-world situations.</a>
+               </div>
+               
+               <div className="cd-hd">
+               <h2 class="card__title-1">School of Automation </h2>
+
+               </div> 
+               </article>
+
+
+
+               <article class="card__article">
+               <img src="assets/dept_img/aapaji.jpeg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">Center For Mathematical Science</span>
+                  <h2 class="card__title"> Aapaji Sansthan</h2>
+                  <a href="#" class="card__button">Computer Science is the core of Faculty of Mathematics and Computing. The department operates from the building "ICICI Centre for Advanced Studies in Computer Science". This building has been constructed from the generous financial assistance from ICICI.</a>
+               </div>
+              
+
+               <div className="cd-hd">
+               <h2 class="card__title-1"> Aapaji Sansthan</h2>
+
+               </div> 
+
+               </article>
+
+               
+               <article class="card__article">
+               <img src="assets/dept_img/automation.jpeg" alt="image" class="card__img"></img>
+               <div class="card__data">
+                  <span class="card__description">Electrical and Electronics,Electronics and Instrumentation</span>
+                  <h2 class="card__title">School of Automation </h2>
+                  <a href="#" class="card__button">The School of Automation at Banasthali is quintessence of the Universitys relentless pursuit of excellence, where the students are exposed to real-world situations.</a>
+               </div>
+               <div className="cd-hd">
+               <h2 class="card__title-1">School of Automation </h2>
+               </div>
+               
+               </article>
+
+
+
+               <article class="card__article">
+               <img src="assets/dept_img/aapaji.jpeg" alt="image" class="card__img"></img>
+
+               <div class="card__data">
+                  <span class="card__description">Center For Mathematical Science</span>
+                  <h2 class="card__title"> Aapaji Sansthan</h2>
+                  <a href="#" class="card__button">Computer Science is the core of Faculty of Mathematics and Computing. The department operates from the building "ICICI Centre for Advanced Studies in Computer Science". This building has been constructed from the generous financial assistance from ICICI.</a>
+               </div>
+               
+
+               <div className="cd-hd">
+               <h2 class="card__title-1"> Aapaji Sansthan</h2>
+               </div>
+               </article>
+
+
+      </div>
+    </div>
+        </div>
+      
+    </div>
+
+    </section>
+
+    </>
+   
+   
+    
   )
 }
 
